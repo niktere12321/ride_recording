db03aa16a2faad70c63f0186f4e2d2904ba70036
Никита <nikterekhoff4242@gmail.com>
2022-05-10 18:12:58 +0300

    test

--- ride/ride/settings.py
+++ ride/ride/settings.py
@@ -93,9 +93,9 @@ AUTH_USER_MODEL = 'users.User'
 
 STATIC_URL = '/staticfiles/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_files')]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_files')]
 
 MEDIA_URL = '/media/'
 
