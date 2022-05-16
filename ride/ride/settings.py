0e553554d63bb86be4cdf78aed5853cb3ccaac52
Никита <nikterekhoff4242@gmail.com>
2022-05-16 22:39:41 +0300

    norm3.0

--- ride/ride/settings.py
+++ ride/ride/settings.py
@@ -92,9 +92,9 @@ USE_TZ = True
 
 AUTH_USER_MODEL = 'users.User'
 
-STATIC_URL = '/staticfiles/'
+STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 #STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_files')]
 
