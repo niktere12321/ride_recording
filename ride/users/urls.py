28b0052595bd21c4e6d7d22b4282cf821b025a25
Никита <nikterekhoff4242@gmail.com>
2022-05-19 21:40:25 +0300

    norm4.0

--- ride/users/urls.py
+++ ride/users/urls.py
@@ -13,10 +13,10 @@ from . import views
 app_name = 'users'
 
 urlpatterns = [
-    path('edd_user/<str:username>', views.edd_user, name='edd_user'),
-    path('users_help', views.users_help, name='users_help'),
-    path('help_active', views.help_active, name='help_active'),
-    path('add_new_user', views.add_new_user, name='add_new_user'),
+    path('edd_user/<str:username>/', views.edd_user, name='edd_user'),
+    path('users_help/', views.users_help, name='users_help'),
+    path('help_active/', views.help_active, name='help_active'),
+    path('add_new_user/', views.add_new_user, name='add_new_user'),
     path('logout/',
          LogoutView.as_view(template_name='users/logged_out.html'),
          name='logout'),
