c4282af9062d97c306ce2a3e8ec227816ced4639
Никита <nikterekhoff4242@gmail.com>
2022-06-16 11:56:29 +0300

    pozti

--- ride/ride/settings.py
+++ ride/ride/settings.py
@@ -94,7 +94,7 @@ AUTH_USER_MODEL = 'users.User'
 
 STATIC_URL = '/static/'
 
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
